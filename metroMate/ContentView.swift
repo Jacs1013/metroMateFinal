fcccc67aa1db838e550f611145cee48b48e85b29
Milla Reichenberg <milla.reichenberg@gmail.com>
2024-07-30 14:33:27 -0400

    fixed spacing and links

--- metroMate/ContentView.swift
+++ metroMate/ContentView.swift
@@ -6,46 +6,67 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            VStack{
-                Text("Root view")
+        NavigationStack {
+            VStack {
+            
             }
-            .toolbar{
-                ToolbarItemGroup(placement: .status){
-                    NavigationLink(destination: mapList())
-                    {
-                        Image("mapIcon")
-                            .resizable(resizingMode: .stretch)
-                            .aspectRatio(contentMode: .fit)
-                    }
-                    NavigationLink(destination: locationList())
-                    {
-                        Image("buildingIcon")
-                            .resizable(resizingMode: .stretch)
-                            .aspectRatio(contentMode: .fit)
-                    }
-                    NavigationLink(destination: favoriteList())
-                    {
-                        Image("heartIcon")
-                            .resizable(resizingMode: .stretch)
-                            .aspectRatio(contentMode: .fit)
-                    }
-                    NavigationLink(destination: ContentView())
-                    {
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    NavigationLink(destination: HomeScreenView()) {
                         Image("homeIcon")
-                            .resizable(resizingMode: .stretch)
+                            .resizable()
                             .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                    }
+                    Spacer()
+                    NavigationLink(destination: MapScreenView()) {
+                        Image("mapIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                    }
+                    Spacer()
+                    NavigationLink(destination: LocationScreenView()) {
+                        Image("buildingIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                    }
+                    Spacer()
+                    NavigationLink(destination: FavoriteScreenView()) {
+                        Image("heartIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
                     }
                 }
             }
-            
         }
     }
 }
 
+struct HomeScreenView: View {
+    var body: some View {
+        Text("Home Screen View")
+    }
+}
+struct MapScreenView: View {
+    var body: some View {
+        Text("Map Screen View")
+    }
+}
+struct LocationScreenView: View {
+    var body: some View {
+        Text("Location Screen View")
+    }
+}
+struct FavoriteScreenView: View {
+    var body: some View {
+        Text("Favorite Screen View")
+    }
+}
 #Preview {
     ContentView()
 }
