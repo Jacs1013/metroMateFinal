b695a9bd87e3fad75249acd66f87740bdf9209bc
Emilee Youn <younemilee@gmail.com>
2024-07-30 13:05:44 -0400

    Initial Commit

---
+++ metroMate/metroMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  metroMateApp.swift
+//  metroMate
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct metroMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
