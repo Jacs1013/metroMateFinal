056b4cb127148cf974055f26f5ee40d7748153b2
Milla Reichenberg <milla.reichenberg@gmail.com>
2024-07-31 10:44:42 -0400

    added the home page

--- metroMate/ContentView.swift
+++ metroMate/ContentView.swift
@@ -10,11 +10,28 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-            
+                Text("MetroMate")
+                    .font(.system(size:40))
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.0, green: 0.16, blue: 0.37, opacity: 1.0))
+                    .padding(.trailing, 100.0)
+                    .padding(.top, -50)
+                    
+                Image("subwayMap")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.leading, .bottom, .trailing], 50)
+                Text("Click the icons below to find individual subway lines, top attractions to visit, and favorite your frequent trips!")
+                    .font(.system(size:20))
+                    .lineSpacing(8)
+                    .font(.subheadline)
+                    .foregroundColor(Color(red: 0.0, green: 0.16, blue: 0.37, opacity: 1.0))
+                    .padding(.horizontal, 50.0)
             }
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
-                    NavigationLink(destination: HomeScreenView()) {
+                    NavigationLink(destination: ContentView()) {
                         Image("homeIcon")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
